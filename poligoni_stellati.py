732e4746d787f8ba2a83433f03019d4883fa63b6
isabellaramani <isabella.ramani@gmail.com>
2026-02-20 13:54:02 +0100

    Lessico in inglese e funzioni di test.

--- poligoni_stellati.py
+++ poligoni_stellati.py
@@ -1,210 +1,178 @@
 import math
 
 
-class Vertice:
-    def __init__(self, nome, x, y):
-        self.nome = nome  # Es: "v1", "v2"
+class Vertex:
+    def __init__(self, name: str, x: float, y: float) -> None:
+        """
+        Inizializza un vertice con nome, coordinate (x, y)
+        e calcola l'angolo rispetto all'origine.
+        """
+        self.name = name  # Es: "v1", "v2"
         self.x = float(x)
         self.y = float(y)
-        # Calcolo automatico dell'angolo (ex theta)
-        # atan2 gestisce correttamente i quadranti (-PI a +PI)
-        self.angolo = math.atan2(self.y, self.x)
+        # atan2 restituisce l'angolo fra -pi e pi
+        self.angle = math.atan2(self.y, self.x)
 
-    def cambio_coordinate(self, x, y):
+    def change_coordinates(self, x: float, y: float) -> None:
         """
         Sposta il vertice a nuove coordinate (x, y)
-        e ricalcola l'angolo automaticamente.
+        e ricalcola l'angolo.
         """
         self.x = x
         self.y = y
-        self.angolo = math.atan2(y, x)
+        self.angle = math.atan2(y, x)
 
-    def cambio_angolo(self, nuovo_angolo):
+    def change_angle(self, new_angle: float) -> None:
         """
         Sposta il vertice sulla circonferenza unitaria (R=1)
         in base al nuovo angolo fornito.
         """
-        self.angolo = nuovo_angolo
-        self.x = math.cos(nuovo_angolo)
-        self.y = math.sin(nuovo_angolo)
+        self.angle = new_angle
+        self.x = math.cos(new_angle)
+        self.y = math.sin(new_angle)
 
 
-origine = Vertice("origine", 0, 0)
+def get_determinant(
+    V1: 'Vertex',
+    V2: 'Vertex',
+    V3: 'Vertex',
+    V4: 'Vertex'
+) -> float:
+    """
+    Calcola il determinante dei vettori V1-V2 e V3-V4.
+     """
+    return ((V1.x - V2.x) * (V3.y - V4.y) -
+            (V1.y - V2.y) * (V3.x - V4.x))
 
-# Il poligono è una lista di vertici.
+
+origine = Vertex("Origine", 0, 0)
 
 
-class Poligono:
-    def __init__(self, vertici=None):
-        if vertici is None:
-            self.vertici = []
+class Polygon:
+    def __init__(self, vertices: list[Vertex] = None) -> None:
+        """
+        Inizializza un poligono come una lista di vertici.
+        """
+        # Se non viene fornita una lista di vertici
+        # inizializza un poligono vuoto.
+        if vertices is None:
+            self.vertices = []
         else:
-            self.vertici = vertici  # Lista di oggetti Vertice
+            self.vertices = vertices
 
-    # Per inizializzare un poligono (diverso da T_0 !)
-    def aggiungi_vertice(self, v):
-        self.vertici.append(v)
+    def add_vertex(self, V: Vertex) -> None:
+        """
+        Aggiunge un vertice al poligono.
+        """
+        self.vertices.append(V)
 
-    # Restituisce il vertice i-esimo (1-based)
-    def v(self, i):
-        n = len(self.vertici)
+    def get_v_i(self, i: int) -> Vertex:
+        """
+        Restituisce il vertice v_i, con i che parte da 1.
+        """
+        n = len(self.vertices)
+        # Gestisce il caso poligono vuoto.
         if n == 0:
-            return None  # Gestione caso poligono vuoto
-        indice_modulato = (i-1) % n
-        return self.vertici[indice_modulato]
+            return None
+        return self.vertices[(i-1) % n]
 
-    def calcola_angolo_di_rotazione(self, i):
+    def get_rotation_angle(self, i: int) -> float:
         """
-        Calcola l'angolo di rotazione necessario per spostare v_i in v_{i+1}.
+        Calcola l'angolo di rotazione di v_i.
         """
-        v_i = self.v(i)
-        v_ip1 = self.v(i+1)
-        v_im1 = self.v(i-1)
+        V_i = self.get_v_i(i)
+        V_ip1 = self.get_v_i(i+1)
+        V_im1 = self.get_v_i(i-1)
 
-        valore_assoluto_rot_vi = math.arccos(((v_i.x-v_im1.x) * (v_ip1.x-v_i.x) + (v_i.y-v_im1.y) * (v_ip1.y-v_i.y)) / (
-            math.sqrt((v_i.x-v_im1.x)**2 + (v_i.y-v_im1.y)**2) * math.sqrt((v_ip1.x-v_i.x)**2 + (v_ip1.y-v_i.y)**2)))
-        determinante = (v_ip1.x - v_i.x) * (v_i.y - v_im1.y) - \
-            (v_ip1.y - v_i.y) * (v_i.x - v_im1.x)
-        rot_vi = math.copysign(valore_assoluto_rot_vi, determinante)
+        # Calcolo dei vettori v_im1->v_i e v_i->v_ip1.
+        vec1_x = V_i.x - V_im1.x
+        vec1_y = V_i.y - V_im1.y
+        vec2_x = V_ip1.x - V_i.x
+        vec2_y = V_ip1.y - V_i.y
+
+        # Calcolo prodotto scalare.
+        dot_product = vec1_x * vec2_x + vec1_y * vec2_y
+        norm_1 = math.sqrt(vec1_x**2 + vec1_y**2)
+        norm_2 = math.sqrt(vec2_x**2 + vec2_y**2)
+
+        abs_value_rot_vi = math.acos(dot_product / (norm_1 * norm_2))
+        det = vec2_x * vec1_y - vec2_y * vec1_x
+        rot_vi = math.copysign(abs_value_rot_vi, det)
         return rot_vi
 
-    def calcola_numero_di_avvolgimento(self, i):
-        somma_rotazioni = 0
-        for i in range(1, len(self.vertici)):
-            rot_vi = self.calcola_angolo_di_rotazione(i)
-            somma_rotazioni = somma_rotazioni + rot_vi
-        numero_di_avvolgimento = somma_rotazioni / (2 * math.pi)
-        return numero_di_avvolgimento
+    def get_winding_number(self) -> int:
+        """
+        Calcola l'indice di avvolgimento del poligono.
+        """
+        sum_rotation_angles = 0
+        # Calcola la somma degli angoli di rotazione
+        # per tutti i vertici del poligono.
+        for j in range(1, len(self.vertices)+1):
+            rot_vj = self.get_rotation_angle(j)
+            sum_rotation_angles = sum_rotation_angles + rot_vj
+        winding_number = sum_rotation_angles / (2 * math.pi)
+        return round(winding_number)
 
-    def verifica_svolta_a_sinistra(self, i):
-        if self.calcola_angolo_di_rotazione(i) > 0:
+    def is_left_turn(self, i: int) -> bool:
+        """
+        Verifica se in v_i svolta a sinistra.
+        """
+        if self.get_rotation_angle(i) > 0:
             return True
         else:
             return False
 
-    def verifica_regolarita_traslazione(self, i, vertice):
+    def center_polygon(self):
         """
-        Verifica se il vertice non appartiene ai coni interdetti di v_i.
+        Centra il poligono spostando tutti i vertici
+        in modo che il centro di massa sia nell'origine (0, 0).
         """
-        v_i = self.v(i)
-        v_im1 = self.v(i-1)
-        v_ip1 = self.v(i+1)
-        v_im2 = self.v(i-2)
-        v_ip2 = self.v(i+2)
-
-        def appartiene_al_cono(v, p, o, a, b):
-            """
-            Verifica se il vertice p appartiene al cono tra a e b, con origine o contenente v.
-            """
-            def det(u_x, u_y, v_x, v_y):
-                return u_x * v_y - u_y * v_x
-
-            # Vettori OA, OB e OV
-            OA_x = a.x - o.x
-            OA_y = a.y - o.y
-            OB_x = b.x - o.x
-            OB_y = b.y - o.y
-            OV_x = v.x - o.x
-            OV_y = v.y - o.y
-            OP_x = p.x - o.x
-            OP_y = p.y - o.y
-
-            # Calcola i determinanti per verificare la posizione di OV rispetto a OA e OB
-            det_OA_OB = det(OA_x, OA_y, OB_x, OB_y)
-            det_OA_OV = det(OA_x, OA_y, OV_x, OV_y)
-            det_OB_OV = det(OB_x, OB_y, OV_x, OV_y)
-            det_OA_OP = det(OA_x, OA_y, OP_x, OP_y)
-            det_OB_OP = det(OB_x, OB_y, OP_x, OP_y)
-
-            # Caso cono convesso
-            if det_OA_OV*det_OA_OB < 0:
-                if det_OA_OP < 0:
-                    return True
-
-            # Caso cono concavo
-            if det_OA_OV*det_OA_OB > 0:
-                return 1
-
-    def centra_poligono(self):
-        """
-        Centra il poligono spostando tutti i vertici in modo che il centro di massa sia nell'origine (0, 0).
-        """
-        n = len(self.vertici)
+        n = len(self.vertices)
         if n == 0:
             return  # Gestione caso poligono vuoto
 
         # Calcola il centro di massa
-        centro_x = sum(v.x for v in self.vertici) / n
-        centro_y = sum(v.y for v in self.vertici) / n
+        center_x = sum(v.x for v in self.vertices) / n
+        center_y = sum(v.y for v in self.vertices) / n
 
         # Sposta ogni vertice in modo che il centro di massa sia all'origine
-        for v in self.vertici:
-            v.cambio_coordinate(v.x - centro_x, v.y - centro_y)
-
-    def trasla_vertici_su_circonferenza_unitaria(self):
-        raggio = max(math.hypot(v.x, v.y) for v in self.vertici)
-        if raggio == 0:
-            return  # Gestione caso poligono con tutti i vertici all'origine
-
-        def trasla_vertice_su_circonferenza(i):
-            # Se la proiezione non appartiene al cono interdetto, proietta il vertice sulla circonferenza
-            v = self.v(i)
-            proiezione_v = Vertice("proiezione_v", 0, 0)
-            proiezione_v_angolo = v.angolo
-            proiezione_v.x = raggio * math.cos(proiezione_v_angolo)
-            proiezione_v.y = raggio * math.sin(proiezione_v_angolo)
-
-            if verifica_regolarita_traslazione(i, proiezione_v) is True:
-                return v.cambio_coordinate(proiezione_v.x, proiezione_v.y)
-            else:
-                # Calcolo punto medio dell'arco tra v_im1 e v_ip2
-                v_im1 = self.v(i-1)
-                v_ip2 = self.v(i+2)
-                punto_medio_arco = Vertice("punto_medio_arco", 0, 0)
-                punto_medio_arco_angolo = (v_im1.angolo + v_ip2.angolo) / 2
-                punto_medio_arco.x = raggio * math.cos(punto_medio_arco_angolo)
-                punto_medio_arco.y = raggio * math.sin(punto_medio_arco_angolo)
-                if verifica_regolarita_traslazione(i, punto_medio_arco) == True:
-                    return v.cambio_coordinate(punto_medio_arco.x, punto_medio_arco.y)
-                # Calcolo punto medio dell'arco tra v_im2 e v_ip1
-                v_im2 = self.v(i-2)
-                v_ip1 = self.v(i+1)
-                punto_medio_arco2 = Vertice("punto_medio_arco2", 0, 0)
-                punto_medio_arco2_angolo = (v_im2.angolo + v_ip1.angolo) / 2
-                punto_medio_arco2.x = raggio * \
-                    math.cos(punto_medio_arco2_angolo)
-                punto_medio_arco2.y = raggio * \
-                    math.sin(punto_medio_arco2_angolo)
-                if verifica_regolarita_traslazione(i, punto_medio_arco2) is True:
-                    return v.cambio_coordinate(punto_medio_arco2.x, punto_medio_arco2.y)
-        for i in range(1, len(self.vertici)):
-            trasla_vertice_su_circonferenza(i)
-
-        # Normalizza le coordinate dei vertici sulla circonferenza unitaria
-        for i in range(1, len(self.vertici)):
-            x = self.v(i).x
-            y = self.v(i).y
-            norma = math.hypot(x, y)
-            self.v(i).cambio_coordinate(x/norma, y/norma)
+        for v in self.vertices:
+            v.change_coordinates(v.x - center_x, v.y - center_y)
 
     # Funzionale solo se i vertici apartengono ad una circonferenza
-    def verifica_ordine_ciclico(self, lista_vertici):
-        n = len(lista_vertici)
+
+    def is_clockwise(self, list_vertices: list[Vertex]) -> bool:
+        """
+        Verifica se i vertici sulla circonferenza unitaria
+        sono ordinati in senso orario.
+        """
+        n = len(list_vertices)
         if n <= 2:
             return True
-        # Verifica se la lista di vertici è ordinata in senso orario
-        for i in range(1, n):
-            v_i = lista_vertici[i-1]
-            v_ip1 = lista_vertici[i]
-            determinante = (v_i.x * v_ip1.y - v_i.y * v_ip1.x)
-            if determinante > 0:
-                return False
-        else:
-            return True
 
-    # Funzione per traslare i vertici del poligono su circonferenza unitaria
+        # Prendo l'angolo del primo vertice come riferimento
+        base_angle = list_vertices[0].angle
+
+        for j in range(1, n-1):
+            current_angle = list_vertices[j].angle
+            # Calcolo la differenza angolare rispetto all'angolo di riferimento
+            # Sarà crescente in senso antiorario
+            current_angle_normalised = (
+                current_angle - base_angle) % (2 * math.pi)
+            next_angle = list_vertices[(j + 1) % n].angle
+            next_angle_normalised = (next_angle - base_angle) % (2 * math.pi)
+
+            # Calcoliamo la differenza fra angoli compresi fra [0, 2pi]
+            diff = (next_angle_normalised - current_angle_normalised)
+
+            # Se diff è positiva percorrendo la circonferenza
+            # a partire dal vertice base i due sono in senso antiorario
+            if diff > 0:
+                return False
+
+        return True
 
 
 # Poligono di prova
-Poligono1 = Poligono((Vertice("v1", 1, 0), Vertice(
-    "v2", 0, 1), Vertice("v3", -1, 0), Vertice("v4", 0, -1)))
+Poligono1 = Polygon((Vertex("v1", 1, 0), Vertex(
+    "v2", 0, 1), Vertex("v3", -1, 0), Vertex("v4", 0, -1)))
--- test.py
+++ test.py
@@ -1,14 +1,117 @@
 import math
-from poligoni_stellati import Vertice
+from poligoni_stellati import Vertex, Polygon, get_determinant
 
 TOLERANCE = 0.0005
+
 
 def is_near(a, b):
     return math.fabs(a - b) < TOLERANCE
 
-def test_cambio_coordinate():
-    V = Vertice("v", 0.5, 0.2)
-    V.cambio_coordinate(1, 2)
+
+def test_change_coordinates():
+    V = Vertex("v", 0.5, 0.2)
+    V.change_coordinates(1, 2)
     assert is_near(V.x, 1)
     assert is_near(V.y, 2)
-    assert is_near(V.angolo, math.atan2(2, 1))
+    assert is_near(V.angle, math.atan2(2, 1))
+
+
+def test_change_angle():
+    V = Vertex("v", 1.5, 2.3)
+    V.change_angle(math.pi / 4)
+    assert is_near(V.angle, math.pi / 4)
+    assert is_near(V.x, math.cos(math.pi / 4))
+    assert is_near(V.y, math.sin(math.pi / 4))
+
+
+def test_get_determinant():
+    V1 = Vertex("V1", 1.3, 2.8)
+    V2 = Vertex("V2", 3.6, 4.1)
+    V3 = Vertex("V3", 5.9, 6.4)
+    V4 = Vertex("V4", 7.3, 8.3)
+    det = get_determinant(V1, V2, V3, V4)
+    expected = (V1.x - V2.x) * (V3.y - V4.y) - (V1.y - V2.y) * (V3.x - V4.x)
+    assert is_near(det, expected)
+
+
+def test_add_vertex():
+    P = Polygon()
+    v = Vertex("V", 1, 2)
+    P.add_vertex(v)
+    assert len(P.vertices) == 1
+    assert is_near(P.vertices[0].x, v.x)
+    assert is_near(P.vertices[0].y, v.y)
+
+
+def test_get_v_i():
+    P = Polygon([Vertex("V1", 5.6, 2.9), Vertex(
+        "V2", 3.2, 4.1), Vertex("V3", 7.6, 1.8)])
+    assert P.get_v_i(2).name == "V2"
+    # DUBBIO: in questo caso deve restituire esattamente 3 e 4?
+    assert is_near(P.get_v_i(2).x, 3.2)
+    assert is_near(P.get_v_i(2).y, 4.1)
+
+
+def test_get_rotation_angle():
+    P = Polygon([Vertex("V1", 1.3, 2.1), Vertex("V2", 2.7, 3.9), Vertex(
+        "V3", 4.9, 1.5), Vertex("V4", 0.5, 0.8), Vertex("V5", 3.5, 1.8)])
+    rot_angle = P.get_rotation_angle(2)
+    # Calcolo vettori V2->V1 e V3->V2
+    vec1_x = P.get_v_i(2).x - P.get_v_i(1).x
+    vec1_y = P.get_v_i(2).y - P.get_v_i(1).y
+    vec2_x = P.get_v_i(3).x - P.get_v_i(2).x
+    vec2_y = P.get_v_i(3).y - P.get_v_i(2).y
+
+    # Calcolo prodotto scalare e norme
+    dot_product = vec1_x * vec2_x + vec1_y * vec2_y
+    norm_1 = math.sqrt(vec1_x**2 + vec1_y**2)
+    norm_2 = math.sqrt(vec2_x**2 + vec2_y**2)
+    abs_value_rot_vi = math.acos(dot_product / (norm_1 * norm_2))
+    det = vec2_x * vec1_y - vec2_y * vec1_x
+    expected_angle = math.copysign(abs_value_rot_vi, det)
+    assert is_near(rot_angle, expected_angle)
+
+
+def test_get_winding_number():
+    # Poligono a farfalla (winding number = 0)
+    P = Polygon([Vertex("V1", -1.4, -1.2), Vertex("V2", 1.7, 1.6),
+                Vertex("V3", 1.3, -1.4), Vertex("V4", -1.5, 1.9)])
+    winding_number = P.get_winding_number()
+    expected_sum = (P.get_rotation_angle(1) + P.get_rotation_angle(2) +
+                    P.get_rotation_angle(3) + P.get_rotation_angle(4))
+    expected_winding_number = expected_sum / (2 * math.pi)
+    assert is_near(winding_number, expected_winding_number)
+
+
+def test_is_left_turn():
+    P = Polygon([Vertex("V1", 3.4, 0), Vertex("V2", 1, 0), Vertex("V3", 1, 1)])
+    expected_rotation_angle = P.get_rotation_angle(2)
+    assert P.is_left_turn(2) == (expected_rotation_angle > 0)
+
+
+def test_center_polygon():
+    P = Polygon([Vertex("V1", 1.5, 2.4), Vertex(
+        "V2", 3.6, 4.1), Vertex("V3", 2.7, 9.8)])
+    coords = [(1.5, 2.4), (3.6, 4.1), (2.7, 9.8)]
+    # Calcolo centro
+    center_x = (1.5 + 3.6 + 2.7) / 3
+    center_y = (2.4 + 4.1 + 9.8) / 3
+    P.center_polygon()
+
+    for i in range(1, 4):
+        expected_x = coords[i-1][0] - center_x
+        expected_y = coords[i-1][1] - center_y
+        assert is_near(P.get_v_i(i).x, expected_x)
+        assert is_near(P.get_v_i(i).y, expected_y)
+
+
+def test_is_clockwise():
+    P_1 = Polygon([Vertex("V1", -1, 0), Vertex("V2", math.sqrt(3) /
+                  2, 0.5), Vertex("V3", 1, 0), Vertex("V4", 0, -1)])
+    assert P_1.is_clockwise(P_1.vertices)
+    P_2 = Polygon([Vertex("V1", 0.5, math.sqrt(3) / 2),
+                  Vertex("V2", -1, 0), Vertex("V3", 0, 1)])
+    assert P_2.is_clockwise(P_2.vertices)
+    P_2 = Polygon([Vertex("V1", 0.5, math.sqrt(3) / 2),
+                  Vertex("V2", -1, 0), Vertex("V3", 0, -1)])
+    assert not P_2.is_clockwise(P_2.vertices)
